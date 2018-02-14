a408c01a4ddea1ddb44d3044b222dcd5183f5fd8
Alberto Soler Muñoz <albertosolermunoz@MacBook-Pro-de-Alberto.local>
2018-02-14 18:38:48 +0100

    DIA 3 cqasi terminado

--- public/js/front_createnew.js
+++ public/js/front_createnew.js
@@ -12,6 +12,15 @@ $(() => {
     baseURL = 'https://api.imgflip.com/get_memes';
     axios.get(baseURL)
          .then((response)=>{
-           console.log(response)
+           console.log(response.data.data.memes[0].url)
+           response.data.data.memes.forEach((p,i) =>{
+            $("#my_image").attr("src","p[i].url")
+            console.log(p);
+        
+           })
+        
+
+
+
          })
 });
