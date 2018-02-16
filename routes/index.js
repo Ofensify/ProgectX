4866110ada63055b4444a36db6f34d5c4ef2345a
Alberto Soler Muñoz <albertosolermunoz@MacBook-Pro-de-Alberto.local>
2018-02-16 14:38:28 +0100

    FiNI

--- routes/index.js
+++ routes/index.js
@@ -96,6 +96,7 @@ router.post('/createnew', (req, res, next) => {
   mailOptions.to = req.body.name;
   Dictionary.find({ 'combination': { $all: combi } })
     .then((offensive) => {
+      console.log(offensive);
       let off = (Math.floor(Math.random(offensive.length)));
       let text0 = offensive[off].text0;
       let text1 = offensive[off].text1;
